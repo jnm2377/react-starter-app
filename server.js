ac8d197b0cf1ccee78d676d0d704fb3cf35ea9ae
Josefina Mancilla <josefinamancilla@josefinas-mbp.austin.ibm.com>
2018-07-23 09:36:27 -0500

    added express-session dependency and middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //THIS IS WHERE WE SET UP  OUR SERVER AND CONNECT IT TO MONGODB
 const express    = require('express');
 const mongoose   = require('mongoose');
+const session    = require('express-session');
 const morgan     = require('morgan');
 const cors 			 = require('cors');
 const bodyParser = require('body-parser');
@@ -12,6 +13,7 @@ const router = express.Router();
 //env variables
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mern-starter';
+const gunny = process.env.GUNNY || 'gunny';
 
 //connecting to mongoose
 mongoose.Promise = Promise;
@@ -21,16 +23,32 @@ db.on( 'error', ( err ) => console.log( err.message + ' is Mongod not running?' 
 db.on( 'connected', () => console.log( 'Mongo OK: ', MONGODB_URI ));
 db.on( 'disconnected', () => console.log( 'Mongo Disconnected' ));
 
+//require routes
+const userRouter = require('./routes/userRoutes.js');
+const sessionRouter = require('./routes/sessionRoutes.js');
+
 //middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('dev'));
+app.use(session({
+  secret: gunny,
+  resave: false,
+  saveUninitialized: false
+}));
+
+//routes
+app.use('/users', userRouter);
+app.use('/sessions', sessionRouter);
 
 app.all('*', (req, res) => {
   console.log('Returning a 404 from the catch-all route');
   return response.sendStatus(404);
 });
+
+
+
 
 
 
